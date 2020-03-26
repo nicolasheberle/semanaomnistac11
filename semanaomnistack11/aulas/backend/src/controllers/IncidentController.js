30d083319a110a89c35d744aadaf9df2fbd3622e
NicolasHeberle <62620939+NicolasHeberle@users.noreply.github.com>
2020-03-26 15:40:41 -0300

    Back-end da aplicação

--- semanaomnistack11/aulas/backend/src/controllers/IncidentController.js
+++ semanaomnistack11/aulas/backend/src/controllers/IncidentController.js
@@ -2,7 +2,25 @@ const connection = require('../database/connection');
 
 module.exports = {
     async index(request, response){
+        const { page = 1 } = request.query;
+
+        const [count] =  await connection('incidents').count();
+
+        console.log(count);
+
         const incidents = await connection('incidents').select('*')
+         .join('ongs', 'ongs_id', '=', 'incidents.ong_id')
+         .limit(5)
+         .offset((page - 1 ) * 5)
+         .select(['incidents.*',
+         'ongs.name',
+         'ongs.email',
+         'ongs.whatsapp',
+         'ongs.city', 
+         'ongs,uf'
+        ]);
+
+        response.header('X-Toal-Count', count['count(*)']);
 
         return response.json(incidents);
     },
