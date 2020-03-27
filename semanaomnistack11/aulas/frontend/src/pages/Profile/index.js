9cbac9aed0d825280b49462d5f165b8022cbe3db
NicolasHeberle <62620939+NicolasHeberle@users.noreply.github.com>
2020-03-27 19:05:26 -0300

    Cases inicialized

--- semanaomnistack11/aulas/frontend/src/pages/Profile/index.js
+++ semanaomnistack11/aulas/frontend/src/pages/Profile/index.js
@@ -1,5 +1,84 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import './style.css';
+
+import logoImg from '../../assets/logo.svg'
 
 export default function Profile(){
-    return <h1>Profile</h1>
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Be The Hero" />
+                <span>Bem vindo, APAD</span>
+
+                <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
+                <button type="button">
+                    <FiPower syze={18} color="#E02041" />
+                </button>
+            </header>
+
+            <h1>Casos cadastrados</h1>
+
+            <ul>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso Teste</p>
+
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR</strong>
+                    <p>R$ 120,00</p>
+                     <FiTrash2 syze={20} color= "#a8a8b3" />
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso Teste</p>
+
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR</strong>
+                    <p>R$ 120,00</p>
+                     <FiTrash2 syze={20} color= "#a8a8b3" />
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso Teste</p>
+
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR</strong>
+                    <p>R$ 120,00</p>
+                     <FiTrash2 syze={20} color= "#a8a8b3" />
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso Teste</p>
+
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR</strong>
+                    <p>R$ 120,00</p>
+                     <FiTrash2 syze={20} color= "#a8a8b3" />
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso Teste</p>
+
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR</strong>
+                    <p>R$ 120,00</p>
+                     <FiTrash2 syze={20} color= "#a8a8b3" />
+                </li>
+            </ul>
+
+        </div>
+    );
 }
