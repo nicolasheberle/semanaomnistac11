0955516b0334f67764c5f18258b5275c5027038b
NicolasHeberle <62620939+NicolasHeberle@users.noreply.github.com>
2020-03-27 12:41:13 -0300

    Register Finalized

--- semanaomnistack11/aulas/frontend/src/pages/Register/index.js
+++ semanaomnistack11/aulas/frontend/src/pages/Register/index.js
@@ -12,22 +12,29 @@ export default function Register(){
             <div className="content">
                 <section>
                     <img src={logoImg} alt="Be The Hero"/>
+                    
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
-                    <Link className="back-link" to ="/register">
-                    <FiArrowLeft size ={16} color='#E02041'/>
-                    Não tenho cadastro
-                </Link>
+                    <Link className="back-link" to ="/">
+                        <FiArrowLeft size ={16} color='#E02041'/>
+                        Não tenho cadastro
+                    </Link>
                 </section>
 
                 <form>
                     <input placeholder="Nome da ONG"/>
                     <input type="email" placeholder="E-Mail" />
                     <input  placeholder="Whatsapp" />
+                    
+                    <div className="input-group">
+                        <input placeholder="Cidade"/>
+                        <input placeholder="UF" style={{ width: 80 }}/>
+                    </div>
 
+                    <button className="button" type="submit">Cadastrar</button>
                 </form>
             </div>
         </div>
-    )
+    );
 }
